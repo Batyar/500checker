bf710c4c9528c91f9fcf353eed2b6dbf16de1dbf
Roman Lazoryshchak <roman.lazoryshchak@onapp.com>
2015-03-16 17:03:34 +0200

    Recursive search

--- lib/responser.rb
+++ lib/responser.rb
@@ -2,23 +2,40 @@ require 'mechanize'
 require 'nokogiri'
 
 class Responser<Mechanize
-  attr_reader :links
+  attr_reader :statuses
+  HEADER = {'Accept' => 'text/html','Content-Type' => 'text/html'}
 
-  def start(link)
-    require 'pry';binding.pry
-    @links = get(link).links.map(&:href)
+  def start(auth_page, login, password)
+  	@statuses = {}
+  	@base_uri = auth_page.sub URI(auth_page).request_uri, ''
+  	add_auth(auth_page, login, password)
+    recursive_search(@base_uri) 
   end
 
-  def open(link)
+  def filter
+  	tmp = {}
+  	self.page.links.map(&:href).compact.uniq.each do |link|
+  	  tmp[@base_uri + link] = nil unless is_exclusion?(link)
+  	end
+  	@statuses = tmp.merge(@statuses)
   end
 
-  def search
+  def is_exclusion?(link)
+  	link == '#' || link.empty? || link =~ /^(http|https)/ 
+  end
+
+  def recursive_search(url)
+  	@statuses[url] = begin
+  		get(url, nil, nil, HEADER).code
+  	rescue Exception => e
+  		e.message
+  	end
+  	proceed = @statuses.select {|k,v| v}.count
+  	puts "URL: #{url}\nStatus: #{@statuses[url]}\nDone: #{proceed}\nLeft: #{@statuses.count - proceed}"
+  	puts '============================================================================================'
+  	filter
+  	@statuses.each do |link, code|
+  		recursive_search(link) unless code
+  	end
   end
 end
-
-
-r = Responser.new
-r.add_auth('http://   /users/sign_in', 'admin', 'changeme')
-r.get('http://   /virtual_machines.json')
-r.start('http://   /virtual_machines')
-require 'pry';binding.pry
